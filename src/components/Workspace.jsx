bca971cf584f8814ae125a4b65de2ab7437228e4
finigami_nishanth <nishanth@finigami.com>
2024-09-25 19:15:43 +0530

    covered basic animations

---
+++ src/components/Workspace.jsx
@@ -0,0 +1,23 @@
+const Workspace=({motionStack})=>{
+return (
+    <div className="bg-blue-400 w-1/2 h-1/2 flex flex-col items-center">
+        Motion stack
+        <div>
+            {
+                motionStack["sprite1"].map((motion,index)=>(
+                    <div key={index} className="flex gap-2">
+                        <div>{motion.type}</div>
+                      {motion.type==='goto'?<div>x: {motion.value.x} y:{motion.value.y}</div>:  <div>{motion.value}</div>}
+                    </div>
+                ))
+            }
+          {/* <div>motion</div>
+          <div>motion</div>
+          <div>motion</div>
+          <div>motion</div> */}
+        </div>
+      </div>
+)
+}
+
+export default Workspace;
